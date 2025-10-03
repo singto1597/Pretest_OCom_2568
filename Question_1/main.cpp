e03329802d456481bb4c3ed6ce55cdc336e12617
Phatthanaphon Sutham <singto1597@gmail.com>
2025-10-03 14:31:59 +0700

    Q1 70/100

--- Question_1/main.cpp
+++ Question_1/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
+    string input;
+    cin >> input;
+    int count = 0;
+    long long minStr = 1000000;
+    string StrLoop = "";
+    for (int i = 1; i <= input.length(); i++){
+        string sub = input.substr(0, i);
+        for (int j = i; j <= input.length(); j += i){
+            if (sub != input.substr(j, i)){
+                count = 0;
+                minStr = 1000000;
+                break;
+            }
+            if (sub == input.substr(j, i)){
+                //cout << input.substr(j, i) << endl;
 
+                if (minStr >= i){
+                    StrLoop = sub;
+                    minStr = i;
+                    count++;
+
+                }
+                if (count > 3){
+                    break;
+                }
+
+            }
+        }
+        if (count > 3){
+            break;
+        }
+    }
+    //count ++;
+    if (StrLoop == ""){
+        StrLoop = input;
+    }
+    cout << StrLoop.length() << " " << StrLoop << " " << input.length()/StrLoop.length() << endl;
 }
