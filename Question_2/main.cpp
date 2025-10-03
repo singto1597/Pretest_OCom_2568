ea2ee7aa8a9949310865557995ff512f48b45e02
Phatthanaphon Sutham <singto1597@gmail.com>
2025-10-03 14:51:09 +0700

    Q2 100/100

--- Question_2/main.cpp
+++ Question_2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
@@ -6,18 +7,20 @@ int main(){
     cin >> n;
     long long perfectPoint = 3, GreatPoint = 2, GoodPoint = 1;
     string BestPerson;
-    long long Bestpoint = -10000;
+    long long Bestpoint = 10000;
     for (int i = 0; i < n; i++){
-        long long perfect, great, good, bad, miss;
+        string perfect;
+        long long great, good, bad, miss;
         long long point;
         string name;
         cin >> name;
         cin >> perfect >> great >> good >> bad >> miss;
-        point = perfect * perfectPoint + great * GreatPoint + good * GoodPoint;
-        if (point > Bestpoint){
-            Bestpoint = point;
-            BestPerson = name;
+        point = great*1 + good*2 + bad*3 + miss*3;
+        if (point < Bestpoint){
+                Bestpoint = point;
+                BestPerson = name;
         }
+
     }
     cout << BestPerson;
 }
