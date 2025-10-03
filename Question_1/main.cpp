5135aa8067216074efb11d452662200cfcae1726
Phatthanaphon Sutham <singto1597@gmail.com>
2025-10-03 16:45:40 +0700

    end of pretest

--- Question_1/main.cpp
+++ Question_1/main.cpp
@@ -5,37 +5,40 @@ using namespace std;
 int main(){
     string input;
     cin >> input;
+    string firstInput = input;
     int count = 0;
     long long minStr = 1000000;
     string StrLoop = "";
-    for (int i = 1; i <= input.length(); i++){
-        string sub = input.substr(0, i);
-        for (int j = i; j <= input.length(); j += i){
-            if (sub != input.substr(j, i)){
-                count = 0;
-                minStr = 1000000;
-                break;
-            }
-            if (sub == input.substr(j, i) && i != 1){
-                //cout << input.substr(j, i) << endl;
-
-                if (minStr >= i){
-                    StrLoop = sub;
-                    minStr = i;
-                    count++;
-
-                }
-                if (count >= 10){
+    for (int number = 1; number <= 2; number++){
+        for (int i = 1; i <= input.length(); i++){
+            string sub = input.substr(0, i);
+            for (int j = i; j <= input.length(); j += i){
+                if (sub != input.substr(j, i)){
+                    count = 0;
+                    minStr = 1000000;
                     break;
                 }
+                if (sub == input.substr(j, i) && i != 1){
+                    //cout << input.substr(j, i) << endl;
 
+                    if (minStr >= i){
+                        StrLoop = sub;
+                        minStr = i;
+                        count++;
+
+                    }
+                    if (count >= 10){
+                        break;
+                    }
+
+                }
+            }
+            if (count >= 10){
+                break;
             }
         }
-        if (count >= 10){
-            break;
-        }
+        input = StrLoop;
     }
-
 
 
 
@@ -43,5 +46,5 @@ int main(){
     if (StrLoop == ""){
         StrLoop = input;
     }
-    cout << StrLoop.length() << " " << StrLoop << " " << input.length()/StrLoop.length() << endl;
+    cout << StrLoop.length() << " " << StrLoop << " " << firstInput.length()/StrLoop.length() << endl;
 }
